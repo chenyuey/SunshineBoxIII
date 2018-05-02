c172ced255db04bd47ed634df222eb5fe7825314
邵励治 <shaolizhi1234@126.com>
2018-05-02 14:31:02 +0800

    首页subject和tag的点击事件

--- app/src/main/java/com/tipchou/sunshineboxiii/ui/index/IndexActivity.kt
+++ app/src/main/java/com/tipchou/sunshineboxiii/ui/index/IndexActivity.kt
@@ -16,20 +16,62 @@ import kotlinx.android.synthetic.main.activity_index_content.*
  */
 class IndexActivity : BaseActivity() {
 
-    //click menu button
-    private fun clickMenuButton() {
-        val drawerLockMode = index_act_drawerlayout.getDrawerLockMode(GravityCompat.START)
-        if (index_act_drawerlayout.isDrawerVisible(GravityCompat.START) && drawerLockMode != DrawerLayout.LOCK_MODE_LOCKED_OPEN) {
-            index_act_drawerlayout.closeDrawer(GravityCompat.START)
-        } else if (drawerLockMode != DrawerLayout.LOCK_MODE_LOCKED_CLOSED) {
-            index_act_drawerlayout.openDrawer(GravityCompat.START)
-        }
-    }
-
     private fun setUpClickEvent() {
         //drawer layout
         index_act_imagebuttton1.setOnClickListener {
-            clickMenuButton()
+            val drawerLockMode = index_act_drawerlayout.getDrawerLockMode(GravityCompat.START)
+            if (index_act_drawerlayout.isDrawerVisible(GravityCompat.START) && drawerLockMode != DrawerLayout.LOCK_MODE_LOCKED_OPEN) {
+                index_act_drawerlayout.closeDrawer(GravityCompat.START)
+            } else if (drawerLockMode != DrawerLayout.LOCK_MODE_LOCKED_CLOSED) {
+                index_act_drawerlayout.openDrawer(GravityCompat.START)
+            }
+        }
+        //subject button
+        index_act_linearlayout1.setOnClickListener {
+            makeEveryBodyGray()
+            index_act_textview1.setTextColor(Color.parseColor("#f09038"))
+            index_act_imageview1.background = getDrawable(R.drawable.ic_nursery_orange)
+        }
+        index_act_linearlayout2.setOnClickListener {
+            makeEveryBodyGray()
+            index_act_textview2.setTextColor(Color.parseColor("#f09038"))
+            index_act_imageview2.background = getDrawable(R.drawable.ic_music_orange)
+        }
+        index_act_linearlayout3.setOnClickListener {
+            makeEveryBodyGray()
+            index_act_textview3.setTextColor(Color.parseColor("#f09038"))
+            index_act_imageview3.background = getDrawable(R.drawable.ic_reading_orange)
+        }
+        index_act_linearlayout4.setOnClickListener {
+            makeEveryBodyGray()
+            index_act_textview4.setTextColor(Color.parseColor("#f09038"))
+            index_act_imageview4.background = getDrawable(R.drawable.ic_game_orange)
+        }
+        //tag button
+        index_act_linearlayout5.setOnClickListener {
+            makeEveryBodyGray()
+            index_act_textview5.setTextColor(Color.parseColor("#f09038"))
+            index_act_imageview5.background = getDrawable(R.drawable.ic_health_orange)
+        }
+        index_act_linearlayout6.setOnClickListener {
+            makeEveryBodyGray()
+            index_act_textview6.setTextColor(Color.parseColor("#f09038"))
+            index_act_imageview6.background = getDrawable(R.drawable.ic_language_orange)
+        }
+        index_act_linearlayout7.setOnClickListener {
+            makeEveryBodyGray()
+            index_act_textview7.setTextColor(Color.parseColor("#f09038"))
+            index_act_imageview7.background = getDrawable(R.drawable.ic_social_orange)
+        }
+        index_act_linearlayout8.setOnClickListener {
+            makeEveryBodyGray()
+            index_act_textview8.setTextColor(Color.parseColor("#f09038"))
+            index_act_imageview8.background = getDrawable(R.drawable.ic_science_orange)
+        }
+        index_act_linearlayout9.setOnClickListener {
+            makeEveryBodyGray()
+            index_act_textview9.setTextColor(Color.parseColor("#f09038"))
+            index_act_imageview9.background = getDrawable(R.drawable.ic_art_orange)
         }
     }
 
@@ -62,26 +104,51 @@ class IndexActivity : BaseActivity() {
         })
     }
 
-    private fun makeEveryBodyBlack() {
-        index_act_textview1.setTextColor(Color.parseColor("#666666"))
-        index_act_textview2.setTextColor(Color.parseColor("#666666"))
-        index_act_textview3.setTextColor(Color.parseColor("#666666"))
-        index_act_textview4.setTextColor(Color.parseColor("#666666"))
-        index_act_textview5.setTextColor(Color.parseColor("#666666"))
-        index_act_textview6.setTextColor(Color.parseColor("#666666"))
-        index_act_textview7.setTextColor(Color.parseColor("#666666"))
-        index_act_textview8.setTextColor(Color.parseColor("#666666"))
-        index_act_textview9.setTextColor(Color.parseColor("#666666"))
-        index_act_imageview1.background = resources.getDrawable(R.drawable.ic_nursery_black)
-        index_act_imageview2.background = resources.getDrawable(R.drawable.ic_music_black)
-        index_act_imageview3.background = resources.getDrawable(R.drawable.ic_reading_black)
-        index_act_imageview4.background = resources.getDrawable(R.drawable.ic_game_black)
-        index_act_imageview5.background = resources.getDrawable(R.drawable.ic_health_black)
-        index_act_imageview6.background = resources.getDrawable(R.drawable.ic_language_black)
-        index_act_imageview7.background = resources.getDrawable(R.drawable.ic_social_black)
-        index_act_imageview8.background = resources.getDrawable(R.drawable.ic_science_black)
-        index_act_imageview9.background = resources.getDrawable(R.drawable.ic_art_black)
+    private fun makeEveryBodyGray() {
+        if (index_act_textview1.currentTextColor == Color.parseColor("#f09038")) {
+            index_act_textview1.setTextColor(Color.parseColor("#666666"))
+            index_act_imageview1.background = getDrawable(R.drawable.ic_nursery_black)
+        }
 
+        if (index_act_textview2.currentTextColor == Color.parseColor("#f09038")) {
+            index_act_textview2.setTextColor(Color.parseColor("#666666"))
+            index_act_imageview2.background = getDrawable(R.drawable.ic_music_black)
+        }
+
+        if (index_act_textview3.currentTextColor == Color.parseColor("#f09038")) {
+            index_act_textview3.setTextColor(Color.parseColor("#666666"))
+            index_act_imageview3.background = getDrawable(R.drawable.ic_reading_black)
+        }
+
+        if (index_act_textview4.currentTextColor == Color.parseColor("#f09038")) {
+            index_act_textview4.setTextColor(Color.parseColor("#666666"))
+            index_act_imageview4.background = getDrawable(R.drawable.ic_game_black)
+        }
+
+        if (index_act_textview5.currentTextColor == Color.parseColor("#f09038")) {
+            index_act_textview5.setTextColor(Color.parseColor("#666666"))
+            index_act_imageview5.background = getDrawable(R.drawable.ic_health_black)
+        }
+
+        if (index_act_textview6.currentTextColor == Color.parseColor("#f09038")) {
+            index_act_textview6.setTextColor(Color.parseColor("#666666"))
+            index_act_imageview6.background = getDrawable(R.drawable.ic_language_black)
+        }
+
+        if (index_act_textview7.currentTextColor == Color.parseColor("#f09038")) {
+            index_act_textview7.setTextColor(Color.parseColor("#666666"))
+            index_act_imageview7.background = getDrawable(R.drawable.ic_social_black)
+        }
+
+        if (index_act_textview8.currentTextColor == Color.parseColor("#f09038")) {
+            index_act_textview8.setTextColor(Color.parseColor("#666666"))
+            index_act_imageview8.background = getDrawable(R.drawable.ic_science_black)
+        }
+
+        if (index_act_textview9.currentTextColor == Color.parseColor("#f09038")) {
+            index_act_textview9.setTextColor(Color.parseColor("#666666"))
+            index_act_imageview9.background = getDrawable(R.drawable.ic_art_black)
+        }
     }
 
     override fun resume() {
