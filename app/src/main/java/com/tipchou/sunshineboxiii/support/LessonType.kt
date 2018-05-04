33a7be94d5df0e19802be5bed6894782bc2ee43b
邵励治 <shaolizhi1234@126.com>
2018-05-04 16:07:50 +0800

    （1）添加了Snackbar （2）添加了LessonType （3）添加了被观察的数据LessonType

---
+++ app/src/main/java/com/tipchou/sunshineboxiii/support/LessonType.kt
@@ -0,0 +1,9 @@
+package com.tipchou.sunshineboxiii.support
+
+/**
+ * Created by 邵励治 on 2018/5/4.
+ * Perfect Code
+ */
+enum class LessonType {
+    NURSERY, MUSIC, READING, GAME, HEALTH, LANGUAGE, SOCIAL, SCIENCE, ART
+}
