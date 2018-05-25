3fdd1db21592134c3cd5cf75160914dec51e60a1
邵励治 <shaolizhi1234@126.com>
2018-05-26 03:44:38 +0800

    专题开头

---
+++ app/src/main/java/com/tipchou/sunshineboxiii/ui/special/SpecialActivity.kt
@@ -0,0 +1,18 @@
+package com.tipchou.sunshineboxiii.ui.special
+
+import android.os.Bundle
+import com.tipchou.sunshineboxiii.R
+import com.tipchou.sunshineboxiii.ui.base.BaseActivity
+
+class SpecialActivity : BaseActivity() {
+    override fun created(bundle: Bundle?) {
+
+    }
+
+    override fun resume() {
+
+    }
+
+    override fun layoutId(): Int = R.layout.activity_special
+
+}
---
+++ app/src/main/java/com/tipchou/sunshineboxiii/ui/special/SpecialViewModel.java
@@ -0,0 +1,8 @@
+package com.tipchou.sunshineboxiii.ui.special;
+
+/**
+ * Created by 邵励治 on 2018/5/26.
+ * Perfect Code
+ */
+public class SpecialViewModel {
+}
